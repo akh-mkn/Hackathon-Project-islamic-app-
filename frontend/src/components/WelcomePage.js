9d8ce3565fea28d07e25d04769af65dcddd9d16c
akh-mkn <mattikn01@gmail.com>
2024-12-11 12:34:52 +0000

    a few new CSS design implementations

--- frontend/src/components/WelcomePage.js
+++ frontend/src/components/WelcomePage.js
@@ -5,8 +5,8 @@ import './WelcomePage.css';
 function WelcomePage() {
     return (
         <div style={{ textAlign: "center", marginTop: "50px" }}>
-            <h1>Welcome to the Islamic Multi-Feature App</h1>
-            <p>Select a feature below:</p>
+            <h1>welcome to the Islamic Multi-Feature App.</h1>
+            <p>select a feature below:</p>
             <div>
                 <Link to="/supplication-counter">
                     <button style={{ margin: "10px" }}>Supplication Counter</button>
