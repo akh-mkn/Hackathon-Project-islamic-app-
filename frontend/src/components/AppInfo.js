dffc2ed13e36409f1fb3c470541f94e57003d1d0
akh-mkn <mattikn01@gmail.com>
2024-12-18 16:07:02 +0000

    Added bodies of text to the App Info and the About me pages.

--- frontend/src/components/AppInfo.js
+++ frontend/src/components/AppInfo.js
@@ -4,7 +4,11 @@ function AboutMe() {
   return (
     <div>
       <h2>App Info</h2>
-      <p>This page introduces the purpose of the app into more detail.</p>
+      <p>I decided to take a more spiritual route for my application! There are several religous based applications available to the public, however I decided to create an app which makes use of a combination of several features, not only from a religious standpoint, but also from a learning standpoint in terms of what we have learnt over the bootcamp, and trying to implement it. </p>
+      <p>My religion is something which is very special to me, and has a large precedence in my purpose, so I thought it would be fun to create an application, which can be built upon in the future, that integrates different things which one may seek for across numerous apps.</p>
+      <p>You may notice particular features to be quite basic, such as the supplication counter! Or maybe, the daily prayer tracker. In truth, I had limited time, however, the way the project is structured, is that I can always revist in the future to continue honing out different parts of it.</p>
+      <br></br>
+      <p>p.s: my personal favourite feature of the application is the music player. If you haven't seen it already, be sure to click the little button in the bottom right!</p>    
     </div>
   );
 }
